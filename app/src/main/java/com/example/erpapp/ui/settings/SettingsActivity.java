6d4a73d2e959c8389deb265a84f7330b7bb2af0e
nathan tugume <41728039+nathantugume@users.noreply.github.com>
2023-09-18 18:39:08 +0300

    updated everything with companyId

--- app/src/main/java/com/example/erpapp/ui/settings/SettingsActivity.java
+++ app/src/main/java/com/example/erpapp/ui/settings/SettingsActivity.java
@@ -1,15 +1,18 @@
 package com.example.erpapp.ui.settings;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
@@ -17,8 +20,16 @@ import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
+import com.example.erpapp.Admin.AdminDashboardActivity;
 import com.example.erpapp.Classes.SettingsManager;
 import com.example.erpapp.R;
+import com.example.erpapp.ui.categories.CategoryActivity;
+import com.example.erpapp.ui.expenses.AccountsActivity;
+import com.example.erpapp.ui.products.ProductsActivity;
+import com.example.erpapp.ui.reports.ReportsActivity;
+import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 import com.google.android.material.switchmaterial.SwitchMaterial;
 import java.util.Map;
 
@@ -43,6 +54,42 @@ public class SettingsActivity extends AppCompatActivity {
         stockLayout = findViewById(R.id.minimumStockLayout);
         sharedPreferences = this.getSharedPreferences("MyPrefs", MODE_PRIVATE);
         companyId = sharedPreferences.getString("companyId", null);
+
+        MaterialToolbar toolbar;
+        //        toolbar
+        toolbar = findViewById(R.id.topAppBar);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener(){
+
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                if(item.getItemId() == R.id.home){
+                    Intent intent = new Intent(SettingsActivity.this, AdminDashboardActivity.class);
+                    startActivity(intent);
+                }else if(item.getItemId() == R.id.category){
+                    Intent intent = new Intent(SettingsActivity.this, CategoryActivity.class);
+                    startActivity(intent);
+                }
+                else if(item.getItemId() == R.id.analytics){
+                    Intent intent = new Intent(SettingsActivity.this, ReportsActivity.class);
+                    startActivity(intent);
+                }
+                else if(item.getItemId() == R.id.product){
+                    Intent intent = new Intent(SettingsActivity.this, ProductsActivity.class);
+                    startActivity(intent);
+                }
+
+                return false;
+            }
+        });
 
         settingsManager = new SettingsManager(companyId,this);
 
